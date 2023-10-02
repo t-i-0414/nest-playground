8103d74e513b34227554ec855fa415fb74229ea7
t-i-0414 <gutlo.ol0414@gmail.com>
2023-10-03 01:31:01 +0900

    feat: controller overview

---
+++ src/cats/dto/index.ts
@@ -0,0 +1,5 @@
+import { CreateCatDto } from './create-cat.dto';
+import { UpdateCatDto } from './update-cat.dto';
+import { ListAllEntities } from './list-all-entities.dto';
+
+export { CreateCatDto, UpdateCatDto, ListAllEntities };
