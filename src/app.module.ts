bafedf2c05d2a62126b673c69190940022f22f69
t-i-0414 <gutlo.ol0414@gmail.com>
2023-10-03 05:20:32 +0900

    feat: pipe tutorial

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CatsModule } from './cats/cats.module';
-import { APP_FILTER } from '@nestjs/core';
+import { APP_FILTER, APP_PIPE } from '@nestjs/core';
 import { HttpExceptionFilter } from './common/exception/http-exception.filter';
 import { AllExceptionsFilter } from './common/exception/all-exceptions.filter';
+import { ValidationPipe } from './common/pipe/validation.pipe';
 
 @Module({
   imports: [CatsModule],
@@ -17,6 +18,10 @@ import { AllExceptionsFilter } from './common/exception/all-exceptions.filter';
     {
       provide: APP_FILTER,
       useClass: HttpExceptionFilter,
+    },
+    {
+      provide: APP_PIPE,
+      useClass: ValidationPipe,
     },
     AppService,
   ],
